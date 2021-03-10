327fc7592623d1d937802cd18a642d935273ccb5
Joris Vial <vialjoris@hotmail.com>
2021-03-10 10:36:53 +0900

    Working websocket integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,120 @@
-#define SDL_MAIN_HANDLED
+//
+// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/boostorg/beast
+//
 
-//Using SDL and standard IO
-#include <SDL.h>
-#include <stdio.h>
+//------------------------------------------------------------------------------
+//
+// Example: WebSocket SSL client, synchronous
+//
+//------------------------------------------------------------------------------
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+#include "root_certificates.hpp"
 
-int main(int argc, char* args[])
+#include <boost/beast/core.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+
+// Sends a WebSocket message and prints the response
+int main(int argc, char** argv)
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
+    try
+    {
+        std::string host = "ws.blockchain.info";
+        auto const  port = "443";
+        auto const  text = "{\"op\":\"unconfirmed_sub\"}";
 
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+        // The io_context is required for all I/O
+        net::io_context ioc;
 
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
+        // The SSL context is required, and holds certificates
+        ssl::context ctx{ ssl::context::tlsv12_client };
 
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+        // This holds the root certificate used for verification
+        load_root_certificates(ctx);
 
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
+        // These objects perform our I/O
+        tcp::resolver resolver{ ioc };
+        websocket::stream<beast::ssl_stream<tcp::socket>> ws{ ioc, ctx };
 
-			//Wait two seconds
-			SDL_Delay(2000);
-		}
-	}
+        // Look up the domain name
+        auto const results = resolver.resolve(host, port);
 
-	//Destroy window
-	SDL_DestroyWindow(window);
+        // Make the connection on the IP address we get from a lookup
+        auto ep = net::connect(get_lowest_layer(ws), results);
 
-	//Quit SDL subsystems
-	SDL_Quit();
+        // Set SNI Hostname (many hosts need this to handshake successfully)
+        if (!SSL_set_tlsext_host_name(ws.next_layer().native_handle(), host.c_str()))
+            throw beast::system_error(
+                beast::error_code(
+                    static_cast<int>(::ERR_get_error()),
+                    net::error::get_ssl_category()),
+                "Failed to set SNI Hostname");
 
-	return 0;
+        // Update the host_ string. This will provide the value of the
+        // Host HTTP header during the WebSocket handshake.
+        // See https://tools.ietf.org/html/rfc7230#section-5.4
+        host += ':' + std::to_string(ep.port());
+
+        // Perform the SSL handshake
+        ws.next_layer().handshake(ssl::stream_base::client);
+
+        // Set a decorator to change the User-Agent of the handshake
+        ws.set_option(websocket::stream_base::decorator(
+            [](websocket::request_type& req)
+            {
+                req.set(http::field::user_agent,
+                    std::string(BOOST_BEAST_VERSION_STRING) +
+                    " websocket-client-coro");
+            }));
+
+        // Perform the websocket handshake
+        ws.handshake(host, "/inv");
+
+        // Send the message
+        ws.write(net::buffer(std::string(text)));
+
+        // This buffer will hold the incoming message
+        beast::flat_buffer buffer;
+
+        // Read a message into our buffer
+        ws.read(buffer);
+
+        // Close the WebSocket connection
+        try {
+            ws.close(websocket::close_code::normal);
+        }
+        catch (std::exception const& e) {
+            // ignore
+        }
+
+        // If we get here then the connection is closed gracefully
+
+        // The make_printable() function helps print a ConstBufferSequence
+        std::cout << beast::make_printable(buffer.data()) << std::endl;
+    }
+    catch (std::exception const& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
